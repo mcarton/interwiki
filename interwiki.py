4d907a5be47a5eb8e734f03a549361fcd560d543
mcarton <cartonmartin+git@gmail.com>
2014-12-05 16:35:14 +0100

    Add xkcd → explainxkcd

--- interwiki.py
+++ interwiki.py
@@ -8,24 +8,48 @@ import urllib.request
 def get_only_in_map(map):
     return next(iter(map.values()))
 
-def get_link(from_lang, site, article, to_lang):
-    url = 'https://%s.%s.org/w/api.php?action=query&continue&format=json&prop=langlinks&titles=%s&lllang=%s' % (from_lang, site, urllib.parse.quote(article), to_lang)
+wikipedia_re = r'https?://..\.wikipedia.org/wiki/.*'
+wiktionary_re = r'https?://..\.wiktionary.org/wiki/.*'
+def wiki(url):
+    def get_article(url):
+        m = re.search(r'https?://(..)\.(.*)\.org/wiki/(.*)', url)
+        return (m.group(1), m.group(2), m.group(3))
 
-    h = urllib.request.urlopen(url)
-    uft8reader = codecs.getreader("utf-8")
-    j = json.load(uft8reader(h))
+    def lang(from_lang):
+        return 'fr' if from_lang == 'en' else 'en'
 
-    n = get_only_in_map(j['query']['pages'])['langlinks'][0]['*']
+    def get_link(from_lang, site, article):
+        to_lang = lang(from_lang)
+        url = 'https://%s.%s.org/w/api.php?action=query&continue&format=json&prop=langlinks&titles=%s&lllang=%s' % (from_lang, site, urllib.parse.quote(article), to_lang)
 
-    return 'https://%s.%s.org/wiki/%s' % (to_lang, site, n)
+        h = urllib.request.urlopen(url)
+        uft8reader = codecs.getreader("utf-8")
+        j = json.load(uft8reader(h))
 
-def get_article(url):
-    m = re.search(r'https?://(..)\.(.*)\.org/wiki/(.*)', url)
-    return (m.group(1), m.group(2), m.group(3))
+        n = get_only_in_map(j['query']['pages'])['langlinks'][0]['*']
 
-def to_lang(from_lang):
-    return 'fr' if from_lang == 'en' else 'en'
+        return 'https://%s.%s.org/wiki/%s' % (to_lang, site, n)
 
-(from_lang, site, article) = get_article(input())
-print(get_link(from_lang, site, article, to_lang(from_lang)))
+    (from_lang, site, article) = get_article(url)
+    return get_link(from_lang, site, article)
+
+xkcd_re = r'https?://xkcd.com/.*'
+def xkcd(url):
+    number = re.search(r'https?://xkcd.com/(.*)/', url)
+
+    if number:
+        return 'http://explainxkcd.com/%s/' % (number.group(1))
+    else:
+        return 'http://explainxkcd.com/'
+
+map = {
+    wikipedia_re: wiki,
+    wiktionary_re: wiki,
+    xkcd_re: xkcd,
+}
+
+url = input()
+for expr in map:
+    if re.search(expr, url):
+        print(map[expr](url))
 
